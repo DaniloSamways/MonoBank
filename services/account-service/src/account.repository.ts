36eb49d88bf8ff026e1d20f4d573ae23d00f7f83
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 19:32:14 -0300

    feat: transfer and getBalance repository methods

--- services/account-service/src/account.repository.ts
+++ services/account-service/src/account.repository.ts
@@ -1,3 +1,22 @@
-export interface IAccountRepository {}
+export interface IAccountRepository {
+  transferFunds(
+    fromAccountId: string,
+    toAccountId: string,
+    amount: number,
+  ): Promise<void>;
+  getBalance(accountId: string): Promise<number>;
+}
 
-export class AccountRepository implements IAccountRepository {}
+export class AccountRepository implements IAccountRepository {
+  transferFunds(
+    fromAccountId: string,
+    toAccountId: string,
+    amount: number,
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  getBalance(accountId: string): Promise<number> {
+    throw new Error("Method not implemented.");
+  }
+}
