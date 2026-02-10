7a3164af267c4b063ecde53e05506346383c1150
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 19:32:19 -0300

    feat: transfer and getBalance controller methods

--- services/account-service/src/account.controller.ts
+++ services/account-service/src/account.controller.ts
@@ -9,10 +9,30 @@ export class AccountController {
     this.accountRepository = accountRepository;
 
     this.healthCheck = this.healthCheck.bind(this);
+    this.transferFunds = this.transferFunds.bind(this);
+    this.getBalance = this.getBalance.bind(this);
   }
 
   async healthCheck(req: Request, res: Response) {
     logger.info({ correlationId: getCorrelationId() }, "healthcheck");
     res.json({ status: "ok" });
+  }
+
+  async transferFunds(req: Request, res: Response) {
+    const { fromAccountId, toAccountId, amount } = req.body;
+    await this.accountRepository.transferFunds(
+      fromAccountId,
+      toAccountId,
+      amount,
+    );
+    res.json({ status: "success" });
+  }
+
+  async getBalance(req: Request, res: Response) {
+    const { accountId } = req.params;
+    const balance = await this.accountRepository.getBalance(
+      accountId as string,
+    );
+    res.json({ balance });
   }
 }
