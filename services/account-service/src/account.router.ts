8945abf7c622501f4439a4f5ef3eacaa782b9f45
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 19:32:27 -0300

    feat: transfer and getBalance routes

--- services/account-service/src/account.router.ts
+++ services/account-service/src/account.router.ts
@@ -7,5 +7,7 @@ const accountRepository = new AccountRepository();
 const accountController = new AccountController(accountRepository);
 
 router.get("/health", accountController.healthCheck);
+router.post("/transfer", accountController.transferFunds);
+router.get("/balance/:accountId", accountController.getBalance);
 
 export default router;
