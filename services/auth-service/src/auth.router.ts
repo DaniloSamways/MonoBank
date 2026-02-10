4f4cbd1101ae4bcf8a46f4e534ff58aa96111e58
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 20:07:16 -0300

    feat: authenticated auth routes

--- services/auth-service/src/auth.router.ts
+++ services/auth-service/src/auth.router.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { AuthRepository } from "./auth.repository";
+import { authMiddleware } from "@monobank/shared";
 
 const router = Router();
 const authRepository = new AuthRepository();
 const authController = new AuthController(authRepository);
 
+router.post("/", authController.createUser);
 router.get("/login", authController.login);
 router.get("/health", authController.healthCheck);
-router.get("/by-email/:email", authController.findUserByEmail);
-router.post("/", authController.createUser);
-router.get("/:id", authController.findUserById);
-router.patch("/:id", authController.updateUser);
-router.delete("/:id", authController.deleteUser);
+router.get(
+  "/by-email/:email",
+  authMiddleware.authenticate,
+  authController.findUserByEmail,
+);
+router.get("/:id", authMiddleware.authenticate, authController.findUserById);
+router.patch("/:id", authMiddleware.authenticate, authController.updateUser);
+router.delete("/:id", authMiddleware.authenticate, authController.deleteUser);
 
 export default router;
