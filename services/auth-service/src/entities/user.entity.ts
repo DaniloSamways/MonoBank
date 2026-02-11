67affac5554830943292d0dfc9efc2357726cba5
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 22:16:04 -0300

    feat: add auth entity

---
+++ services/auth-service/src/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  birthDate!: string;
+
+  @Column()
+  password!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
