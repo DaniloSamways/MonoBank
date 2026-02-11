8d69c8c30a19ff60c1ee245f788f8eea50c6656d
Danilo Samways <hadeflowing@gmail.com>
2026-02-10 22:15:41 -0300

    chore: add typeorm settings

---
+++ services/auth-service/src/db/index.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm";
+import { User } from "../entities/user.entity";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.AUTH_DB_HOST!,
+  port: Number(process.env.AUTH_DB_PORT!),
+  username: process.env.AUTH_DB_USER!,
+  password: process.env.AUTH_DB_PASS!,
+  database: process.env.AUTH_DB_NAME!,
+  entities: [User],
+  synchronize: false,
+  logging: false,
+});
+
+try {
+  AppDataSource.initialize();
+} catch (error) {
+  console.log(error);
+}
